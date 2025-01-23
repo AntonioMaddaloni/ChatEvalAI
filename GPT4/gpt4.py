7d33532635f61d2cda1f0befdcda919fefe08495
Francesco Peluso <francesco.peluso@agtweb.it>
2025-01-23 15:20:03 +0100

    add_dotenv

--- GPT4/gpt4.py
+++ GPT4/gpt4.py
@@ -1,15 +1,21 @@
-from openai import OpenAI
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+
+# Carica le variabili di ambiente dal file .env
+load_dotenv()
 
 #Config OpenAI
-client = OpenAI(api_key="g4a-aMWTFlPKVs2WcFmIY24Cc99XMakJQ9eJQB8", base_url="https://api.gpt4-all.xyz/v1")
+client = AzureOpenAI(api_key=os.getenv('api_key'), azure_endpoint=os.getenv('azure_endpoint'),api_version=os.getenv('api_version'))
 
 #Return Simple Response By User
 def response_by_user(message):
     #def response
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model="gpt-4",
         messages=[{"role": "user", "content": message}],
         stream=False,
     )
     #return response
     return response.choices[0].message.content
+print(response_by_user("ciao"))
